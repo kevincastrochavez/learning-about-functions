aa44c310640ab957c03b8e897be16ad5fbe612ec
kevincastrochavez <kevin.castro.chavez0@gmail.com>
2020-12-26 14:51:33 -0600

    functions returning functions

--- script.js
+++ script.js
@@ -54,19 +54,34 @@
 ///////////////////////////////////
 ///// HIGHER ORDER FUNCTIONS
 
-const oneWord = function (str) {
-  return str.replace(/ /g, '').toLowerCase();
-};
+// const oneWord = function (str) {
+//   return str.replace(/ /g, '').toLowerCase();
+// };
 
-const upperFirstWord = function (str) {
-  const [first, ...others] = str.split(' ');
-  return [first.toUpperCase(), ...others].join(' ');
-};
+// const upperFirstWord = function (str) {
+//   const [first, ...others] = str.split(' ');
+//   return [first.toUpperCase(), ...others].join(' ');
+// };
 
-const transformer = function (str, fn) {
-  console.log(`Original: ${str}`);
-  console.log(`Transform: ${fn(str)}`);
-};
+// const transformer = function (str, fn) {
+//   console.log(`Original: ${str}`);
+//   console.log(`Transform: ${fn(str)}`);
+// };
 
-transformer('Javascript is the best', upperFirstWord);
-transformer('Javascript is the best', oneWord);
+// transformer('Javascript is the best', upperFirstWord);
+// transformer('Javascript is the best', oneWord);
+
+// const greet = function (greeting) {
+//   return function (name) {
+//     console.log(`${greeting} ${name}`);
+//   };
+// };
+
+// const greeterHey = greet('Hey');
+// greeterHey('Jonas');
+// greeterHey('Kevin');
+
+// // WITH ARROW FUNCTIONS
+// const greetArr = greeting => name => console.log(`${greeting} ${name}`);
+
+// greetArr('Hi')('Juan');
