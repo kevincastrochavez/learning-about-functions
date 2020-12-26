fee7e85e079b056435aea430c01cd0168aeb4c63
kevincastrochavez <kevin.castro.chavez0@gmail.com>
2020-12-26 14:03:09 -0600

    default parameters

--- script.js
+++ script.js
@@ -1,1 +1,21 @@
 'use strict';
+
+///////////////////////////////////
+///// DEFAULT PARAMETERS
+
+const bookings = [];
+
+const createBooking = function (flightNum, numPass = 1, price = 199) {
+  const booking = {
+    flightNum,
+    numPass,
+    price,
+  };
+  console.log(booking);
+  bookings.push(booking);
+};
+
+createBooking('LH123');
+createBooking('LH123', 4, 250);
+// When want to skip a parameter, set to undefined
+createBooking('LH123', undefined, 250);
