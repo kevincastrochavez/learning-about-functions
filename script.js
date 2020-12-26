caa534902f703f709f3841706fbe5f66f1df3fd2
kevincastrochavez <kevin.castro.chavez0@gmail.com>
2020-12-26 15:18:44 -0600

    apply and call methods

--- script.js
+++ script.js
@@ -85,3 +85,46 @@
 // const greetArr = greeting => name => console.log(`${greeting} ${name}`);
 
 // greetArr('Hi')('Juan');
+
+///////////////////////////////////
+/////
+
+const lufthansa = {
+  airline: 'Lufthansa',
+  iataCode: 'LH',
+  bookings: [],
+  book(flightNum, name) {
+    console.log(
+      `${name} booked a seat on ${this.airline} flight ${this.iataCode}${flightNum}`
+    );
+    this.bookings.push({ flight: `${this.iataCode}${flightNum}`, name });
+  },
+};
+
+lufthansa.book(239, 'Kevin Castro');
+lufthansa.book(635, 'Cindy Chávez');
+console.log(lufthansa);
+
+const eurowings = {
+  airline: 'Eurowings',
+  iataCode: 'EW',
+  bookings: [],
+};
+
+const book = lufthansa.book;
+// DOES NOT WORK BECAUSE IT IS A REGULAR FUNCTION
+// book(239, 'Jared Castro');
+
+// CALL METHOD
+book.call(eurowings, 102, 'Jared Castro');
+console.log(eurowings);
+
+book.call(lufthansa, 569, 'Alicia Chávez');
+console.log(lufthansa);
+
+//APPLY METHOD
+const flightData = [593, 'Juan Clavino'];
+book.apply(eurowings, flightData);
+console.log(eurowings);
+
+book.call(eurowings, ...flightData);
