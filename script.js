5620daf4e9de0e89fc7228da33c4c5104e0c06ac
kevincastrochavez <kevin.castro.chavez0@gmail.com>
2021-01-04 19:41:18 -0600

    learned more about closures with examples

--- script.js
+++ script.js
@@ -248,9 +248,75 @@ const secureBooking = function () {
   };
 };
 
-const booker = secureBooking();
-booker();
-booker();
+// const booker = secureBooking();
+// booker();
+// booker();
 
-booker();
-console.dir(booker);
+// booker();
+// console.dir(booker); CONSOLE CLOSURES
+
+// let f;
+
+// const g = function () {
+//   const a = 23;
+//   f = function () {
+//     console.log(a * 2);
+//   };
+// };
+
+// const h = function () {
+//   const b = 777;
+//   f = function () {
+//     console.log(b * 2);
+//   };
+// };
+
+// g();
+// f();
+// h();
+// f();
+
+// console.dir(f);
+
+///////////////////////////////////////
+// More Closure Examples
+// Example 1
+let f;
+
+const g = function () {
+  const a = 23;
+  f = function () {
+    console.log(a * 2);
+  };
+};
+
+const h = function () {
+  const b = 777;
+  f = function () {
+    console.log(b * 2);
+  };
+};
+
+g();
+f();
+console.dir(f);
+
+// Re-assigning f function
+h();
+f();
+console.dir(f);
+
+// Example 2
+const boardPassengers = function (n, wait) {
+  const perGroup = n / 3;
+
+  setTimeout(function () {
+    console.log(`We are now boarding all ${n} passengers`);
+    console.log(`There are 3 groups, each with ${perGroup} passengers`);
+  }, wait * 1000);
+
+  console.log(`Will start boarding in ${wait} seconds`);
+};
+
+const perGroup = 1000;
+boardPassengers(180, 3);
